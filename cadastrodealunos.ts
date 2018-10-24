05a6ed6978015991c85c15263dea1f9fb653f933
Lucas Aurelio Gomes Costa <40130323+lucas625@users.noreply.github.com>
2018-10-24 12:05:52 -0300

    Salvando as coisas

---
+++ cadastrodealunos.ts
@@ -0,0 +1,33 @@
+import { Aluno } from '../../gui/ta-gui/src/app/aluno';
+
+export class CadastroDeAlunos {
+  alunos: Aluno[] = [];
+
+  criar(aluno: Aluno): Aluno {
+    var result = null;
+    if (this.cpfNaoCadastrado(aluno.cpf)) {
+      result = new Aluno();
+      result.copyFrom(aluno);
+      this.alunos.push(result);
+    }
+    return result;
+  }
+
+  cpfNaoCadastrado(cpf: string): boolean {
+     return !this.alunos.find(a => a.cpf == cpf);
+  }
+
+  atualizar(aluno: Aluno): Aluno {
+    var result: Aluno = this.alunos.find(a => a.cpf == aluno.cpf);
+    if (result) result.copyFrom(aluno);
+    return result;
+  }
+
+  getAlunos(): Aluno[] {
+    return this.alunos;
+  }
+  deleteAluno(cpf:string){
+    var a:number=this.alunos.findIndex(x => x.cpf==cpf);
+    this.alunos.splice(a,1);
+  }
+}
